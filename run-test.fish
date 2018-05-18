65eeb5c8ecd2524ab4586d3a8a7226e676897709
peterdemartini <thepeterdemartini@gmail.com>
2018-05-18 11:01:52 -0700

    More accurate test

--- run-test.fish
+++ run-test.fish
@@ -1,77 +1,67 @@
 #!/usr/bin/env fish
 
 set jobs_folder "./jobs"
-set total_messages 1000000
+set data_set "fixed-data-set"
+set job_name "$argv[1]-kafka-etl"
+set total_messages 10000000
 
 function _tjm
     tjm $argv > /dev/null 2>&1 | grep -v 'ExperimentalWarning';
 end
 
-function _topic_exists
-    kafka-topics --list --zookeeper localhost:2181 | grep "$argv[1]" > /dev/null;
-end
-
-function _dataset_empty
-    kafka-console-consumer \
-              --bootstrap-server localhost:9092 \
-              --topic "$argv[1]" \
-              --from-beginning \
-              --timeout-ms 1000 \
-              --max-messages=$total_messages > /dev/null ^| grep -v "Processed a total of $total_messages messages" 2>&1 /dev/null;
-end
-
-function _dataset_exists
-    _topic_exists "$argv[1]"; and _dataset_empty "$argv[1]"
-end
-
-function test_kafka_etl
-    if [ "$argv[1]" ];
-        echo "* testing $argv[1]"
-    else
-        echo "Missing first, should be either old or new"
-        exit 1
-    end
-    trap exit SIGINT
-    set -l job_name "$argv[1]-kafka-etl"
-    echo "* stopping old-kafka-etl job"
-    _tjm stop $jobs_folder/old-kafka-etl.json
-    echo "* stopping new-kafka-etl job"
-    _tjm stop $jobs_folder/new-kafka-etl.json
-    if _dataset_exists "$argv[1]";
-        echo "* deleting fix-data-set topic"
+function _setup_dataset
+    if kafka-topics --list --zookeeper localhost:2181 | grep "$data_set" > /dev/null;
+        echo "[*] deleting $data_set topic"
         kafka-topics --zookeeper localhost:2181 \
             --delete \
-            --topic "fixed-data-set" > /dev/null 2> /dev/null; or exit 1
-        _tjm start $jobs_folder/once-data-generator-stream.json
-        echo "* loading data into kafka"
-        while tjm status $jobs_folder/once-data-generator-stream.json 2> /dev/null | grep -v completed > /dev/null;
-            sleep 1;
-        end;
-        _tjm stop $jobs_folder/once-data-generator-stream.json
-        echo "* done loading data into kafka"
-    else
-        echo "* data is already there"
+            --topic "$data_set" > /dev/null 2> /dev/null; or exit 1
     end
-    echo "* deleting topic $job_name"
-    if _topic_exists "$job_name";
+    _tjm start $jobs_folder/once-data-generator-stream.json
+    echo "[*] loading data into kafka"
+end
+
+function _reset_kafka_for_job
+    if kafka-topics --list --zookeeper localhost:2181 | grep "$job_name" > /dev/null;;
+        echo "[*] deleting topic $job_name"
         kafka-topics --zookeeper localhost:2181 \
             --delete \
             --topic "$job_name" > /dev/null 2> /dev/null; or exit 1
     end
-    echo "* creating topic $job_name"
+    echo "[*] creating topic $job_name"
     kafka-topics --zookeeper localhost:2181 \
         --create \
         --topic "$job_name" \
         --partitions=1 \
         --replication-factor=1 > /dev/null 2> /dev/null; or exit 1
-    echo "* resetting offset for topic $job_name"
-    kafka-consumer-groups --bootstrap-server localhost:9092 \
-        --topic "fixed-data-set" \
-        --reset-offsets \
-        --group "$job_name" \
-        --to-earliest \
-        --execute 2> /dev/null; or exit 1
-    echo "* starting test - "(date)
+    # echo "[*] resetting offset for topic $job_name"
+    # kafka-consumer-groups --bootstrap-server localhost:9092 \
+    #     --topic "$data_set" \
+    #     --reset-offsets \
+    #     --group "$job_name" \
+    #     --to-earliest \
+    #     --execute 2> /dev/null; or exit 1
+end
+
+function test_kafka_etl
+    if [ "$job_name" ];
+         echo "[+] Setting up test"
+    else
+        echo "Missing first, should be either old or new"
+        exit 1
+    end
+    function gracefulExit --on-signal INT --on-signal TERM
+        functions -e gracefulExit
+        echo "[X] Cancelled"
+        tjm stop $jobs_folder/$job_name.json 2> /dev/null > /dev/null
+        tjm stop $jobs_folder/once-data-generator-stream.json 2> /dev/null > /dev/null
+        kill %self
+    end
+    echo "[*] ensure jobs are stopped before starting"
+    _tjm stop $jobs_folder/old-kafka-etl.json
+    _tjm stop $jobs_folder/new-kafka-etl.json
+    _reset_kafka_for_job
+    _setup_dataset
+    echo "[+] Starting $job_name -- "(date)
     _tjm start "jobs/$job_name.json"
     set -l start_time (gdate +%s)
     kafka-console-consumer \
@@ -79,12 +69,12 @@ function test_kafka_etl
         --topic "$job_name" \
         --from-beginning \
         --timeout-ms 3600000 \
-        --max-messages=$total_messages > "$HOME/tmp/$job_name.log"
-    echo "* test complete - "(date)
-    set -l end_time (gdate +%s)
-    set -l result (math $end_time - $start_time)
-    echo "* test took $result seconds"
-    _tjm stop $jobs_folder/$job_name.json
+        --max-messages=$total_messages > /dev/null
+    set -l result (math (gdate +%s) - $start_time)
+    echo "[√] Done! test took $result seconds -- "(date)
+    functions -e gracefulExit
+    tjm stop $jobs_folder/$job_name.json 2> /dev/null > /dev/null
+    tjm stop $jobs_folder/once-data-generator-stream.json 2> /dev/null > /dev/null
 end
 
-test_kafka_etl "$argv[1]"
+test_kafka_etl "$job_name"
