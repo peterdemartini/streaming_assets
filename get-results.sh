17340871d0746e9934b64d2f90a5aff4d8ff7d4f
peterdemartini <thepeterdemartini@gmail.com>
2018-05-21 13:21:22 -0700

    better get results function

--- get-results.sh
+++ get-results.sh
@@ -3,35 +3,48 @@
 set -e
 
 bytesToHuman() {
-    b=${1:-0}; d=''; s=0; S=(Bytes {K,M,G,T,E,P,Y,Z})
+    b=${1:-0}; d=''; s=0; S=(Bytes {K,M,G,T,E,P,Y,Z}B)
     while ((b > 1024)); do
-        d="$(printf ".%02d" $((b % 1024 * 100 / 1024)))"
+        d="$(printf "%d" $((b % 1024 * 100 / 1024)))"
         b=$((b / 1024))
         let s++
     done
-    echo "$b$d${S[$s]}"
+    echo "$b$d ${S[$s]}"
+}
+
+get_mem_metrics() {
+    local metrics=("max" "min" "mean" "median")
+    local method="$1"
+    local result
+    local results=()
+    local mem_file="./$method-kafka-mem.log"
+    for metric in ${metrics[@]}; do
+        result="$(datamash --format "%.f" "$metric" 1 < "$mem_file")"
+        results+=("$(bytesToHuman "$result") ($metric)")
+    done
+    echo "[*] $method-kafka-etl CPU: ${results[*]}"
+}
+
+get_cpu_metrics() {
+    local metrics=("max" "min" "mean" "median")
+    local method="$1"
+    local result
+    local results=()
+    local cpu_file="./$method-kafka-cpu.log"
+    for metric in ${metrics[@]}; do
+        result="$(datamash --format "%.f" "$metric" 1 < "$cpu_file")"
+        results+=("$result% ($metric)")
+    done
+    echo "[*] $method-kafka-etl MEM: ${results[*]}"
 }
 
 main() {
-    local methods=("new" "old")
-    local types=("cpu" "mem")
-    local metrics=("max" "min" "mean" "median")
-    for method in ${methods[@]}; do
-        for type in ${types[@]}; do
-            local file="./$method-kafka-$type.log"
-            for metric in ${metrics[@]}; do
-                local result
-                result="$(datamash --format "%.f" "$metric" 1 < "$file")"
-                if [ "$type" == 'mem' ]; then
-                    echo "[*] $method $type $metric $(bytesToHuman "$result")"
-                else
-                    echo "[*] $method $type $metric $result%"
-                fi
-            done
-        done
-    done
-    cat ./new-kafka-etl.log | tail -n 1
-    cat ./new-kafka-etl.log | tail -n 1
+    get_mem_metrics "new"
+    get_mem_metrics "old"
+    get_cpu_metrics "new"
+    get_cpu_metrics "old"
+    cat ./new-kafka-etl.log | grep 'done!'
+    cat ./old-kafka-etl.log | grep 'done!'
 }
 
 main "$@"
--- run-tests.sh
+++ run-tests.sh
@@ -24,18 +24,36 @@ main() {
     new_pid="$!"
     tail -f new-kafka-etl.log &
     tail -f old-kafka-etl.log &
-    echo "[*] waiting for tests to finish..."
     while sleep 1; do
+        local old_done new_done
         ps -p "$old_pid" 2> /dev/null > /dev/null
-        if [ "$?" != "0" ]; then
-            echo "old test is done. Will kill new test in 30 seconds"
-            sleep 30
+        old_done="$?"
+        ps -p "$new_pid" 2> /dev/null > /dev/null
+        new_done="$?"
+        if [ "$old_done" != "0" ] && [ "$new_done" != "0" ]; then
+          echo "[*] both tests are done"
+          break
+        fi
+        if [ "$old_done" != "0" ]; then
+            echo "[*] old test is done. Will kill new test in 30 seconds"
+            for i in {1..30}; do
+              ps -p "$new_pid" 2> /dev/null > /dev/null
+              if [ "$?" != "0" ]; then
+                break
+              fi
+              sleep 1
+            done
             break
         fi
-        ps -p "$new_pid" 2> /dev/null > /dev/null
-        if [ "$?" != "0" ]; then
-            echo "new test is done. Will kill old test in 30 seconds"
-            sleep 30
+        if [ "$new_done" != "0" ]; then
+            echo "[*] new test is done. Will kill old test in 30 seconds"
+            for i in {1..30}; do
+              ps -p "$old_pid" 2> /dev/null > /dev/null
+              if [ "$?" != "0" ]; then
+                break
+              fi
+              sleep 1
+            done
             break
         fi
     done
